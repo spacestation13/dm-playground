b285d09cd58c495b29b667834d64d3534dad5396
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2023-02-06 08:21:12 -0500

    Starts moving everything to a web worker

--- src/vm/emulator.ts
+++ src/vm/emulator.ts
@@ -1,105 +1,58 @@
-import wasmUrl from "../lib/v86.wasm";
-import isoUrl from "val-loader!../../fetchIso.js?raw";
+import { TypedEmitter } from "tiny-typed-emitter";
 
-const emulator = new V86Starter({
-  //Emulator binaries
-  wasm_path: wasmUrl,
-  //Hell is this?
-  acpi: false,
+export interface EmulatorEvents {
+  receivedOutputConsole: (output: string) => void;
+  receivedOutputScreen: (output: string) => void;
+  receivedOutputController: (output: string) => void;
 
-  //Log level, debugging?
-  log_level: 0,
-  //128MB of ram
-  memory_size: 128 * 1024 * 1024,
-  //8MB of video ram
-  vga_memory_size: 8 * 1024 * 1024,
-  //CD / Floppy / HD
-  boot_order: 0x213,
-  //Skips boot menu delay on boch BIOS apparently
-  fastboot: true,
-  //From my understanding, these control serial terminals
-  uart1: true, //Terminal
-  uart2: true, //Screen
-  uart3: true, //Controller
-  //Used for weird ass automatic kernel image loading
-  cmdline: null,
-  //Presumably saves the mac address in the state
-  preserve_mac_from_state_image: true,
-  //Instance of NetworkAdapter
-  network_adapter: null,
-  //URL to websocket proxy (wss://relay.widgetry.org/)
-  network_relay_url: null,
-  disable_keyboard: true,
-  disable_mouse: true,
-  //Canvas element
-  screen_container: document.getElementById("screenOut"),
-  //Fake screen
-  screen_dummy: false,
-  //Textarea element
-  serial_container: null,
-  //Div element
-  serial_container_xtermjs: null,
-  disable_speaker: true,
-  //Images:
-  /**
-   * Can be an object implementing get(), set() and load() or
-   * {
-   *     buffer: ArrayBuffer | File,
-   *     async: boolean | undefined, //Defaults to true if size is over 256M
-   *     url: string, //if buffer is not set, url to load image from
-   *     size: number, //required for async, not sure for sync urls
-   *     use_parts: boolean //used for split up images
-   * }
-   */
+  sentToController: (output: string) => void;
 
-  bios: {
-    url: "https://raw.githubusercontent.com/copy/v86/master/bios/seabios.bin",
-  },
-  vga_bios: {
-    url: "https://raw.githubusercontent.com/copy/v86/master/bios/vgabios.bin",
-  },
-  cdrom: {
-    url: isoUrl,
-  },
-  hda: null,
-  hdb: null,
-  fda: null,
-  fdb: null,
-  initial_state: null,
-  multiboot: null,
-  bzimage: null,
-  initrd: null,
-  //9p filesystem, { basefs: string, baseurl: string } or {}
-  filesystem: {},
-  //Loads bzimage and initrd from 9p filesystem
-  bzimage_initrd_from_filesystem: false,
-  autostart: false,
-});
-
-const sendTerminal = function (data: string) {
-  for (let i = 0; i < data.length; i++) {
-    emulator.bus.send("serial0-input", data.charCodeAt(i));
-  }
-};
-const sendScreen = function (data: string) {
-  for (let i = 0; i < data.length; i++) {
-    emulator.bus.send("serial1-input", data.charCodeAt(i));
-  }
-};
-const sendController = function (data: string) {
-  console.log(data.replace(/\x00/g, "\n---\n< "));
-  //termCtl.write(data.replace(/\x00/g, "\n---\n< "));
-  for (let i = 0; i < data.length; i++) {
-    emulator.bus.send("serial2-input", data.charCodeAt(i));
-  }
-};
-
-function pause() {
-  emulator.stop();
+  resetOutputConsole: () => void;
 }
 
-function unpause() {
-  emulator.run();
-}
+const encoder = new TextEncoder();
 
-export { emulator, pause, unpause, sendController, sendScreen, sendTerminal };
+class Emulator extends TypedEmitter<EmulatorEvents> {
+  private worker = new Worker(new URL("./worker.ts", import.meta.url));
+
+  constructor() {
+    super();
+
+    this.worker.onmessage = ({ data: e }) => {
+      this.emit(e.event, ...(e.data ?? []));
+    };
+  }
+
+  public sendTerminal = (data: string) => {
+    this.worker.postMessage({ command: "sendTerminal", data });
+  };
+  public sendScreen = (data: string) => {
+    this.worker.postMessage({ command: "sendScreen", data });
+  };
+  public sendController = (data: string) => {
+    this.emit("sentToController", data);
+    this.worker.postMessage({ command: "sendController", data });
+  };
+
+  public pause = () => {
+    this.worker.postMessage({ command: "pause" });
+  };
+
+  public start = () => {
+    this.worker.postMessage({ command: "start" });
+  };
+
+  public sendFile = (
+    name: string,
+    content: ArrayBuffer | Uint8Array | string,
+  ) => {
+    if (typeof content === "string") content = encoder.encode(content);
+    content = new Uint8Array(content);
+    this.worker.postMessage({ command: "sendFile", name: name, data: content });
+  };
+
+  public resetTerminal = () => {
+    this.worker.postMessage({ command: "resetTerminal" });
+  };
+}
+export const emulator = new Emulator();
