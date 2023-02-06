18e1f48afcb44e855c27573a7e1a912e466236c9
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2023-02-06 08:24:07 -0500

    Removes provides.ts as we don't use providers anymore in favor of pinia

--- src/provides.ts
+++
@@ -1,9 +0,0 @@
-import { Component, InjectionKey, Ref } from "vue";
-
-export const provideLayout = Symbol("provideLayout") as InjectionKey<{
-  editorElement: Ref<HTMLDivElement | null>;
-  remainingXSpace: Ref<number>;
-  remainingYSpace: Ref<number>;
-  register: (type: "x" | "y", panel: Ref<number>) => void;
-  unregister: (panel: Ref<number>) => void;
-}>;
