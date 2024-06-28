a3ea00195c80d8b18bf43fd80c7a40e6cf7619d9
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2024-06-28 16:24:20 -0400

    useActive in executor

--- src/vm/executor.service.ts
+++ src/vm/executor.service.ts
@@ -3,6 +3,7 @@ import { EventEmitter, Injectable, Output } from '@angular/core';
 import { CommandQueueService } from './commandQueue.service';
 import { EmulatorService } from './emulator.service';
 import { once } from '../utils/misc';
+import { ByondService } from './byond.service';
 
 @Injectable({
   providedIn: 'root',
@@ -16,60 +17,66 @@ export class ExecutorService {
   constructor(
     private commandQueue: CommandQueueService,
     private emulator: EmulatorService,
+    private byondService: ByondService,
   ) {}
 
   public async executeImmediate(
     code: string,
     signal?: AbortSignal,
   ): Promise<void> {
-    const filename = `tmp-${Date.now()}`;
-    let stageAbort: Function | undefined = undefined;
+    return this.byondService.useActive((path) => {
+      if (path == null)
+        return Promise.reject(new Error('No active byond version'));
 
-    this.reset.emit();
-    const promise = new CancelablePromise<void>((resolve) => {
-      resolve(this.emulator.start());
-    })
-      .then(() => {
-        return this.emulator.sendFile(filename + '.dme', code);
-      })
-      .then(() => {
-        return this.commandQueue.runProcess(
-          '/byond/bin/DreamMaker',
-          '/mnt/host/' + filename + '.dme',
-          new Map([['LD_LIBRARY_PATH', '/byond/bin']]),
-        );
-      })
-      .then((compiler) => {
-        this.output.emit('=== Compile stage ===\n');
-        compiler.stdout.subscribe((val) => this.output.emit(val));
-        stageAbort = compiler.kill;
-        return once(compiler.exit);
-      })
-      .then(() => {
-        return this.commandQueue.runProcess(
-          '/byond/bin/DreamDaemon',
-          `/mnt/host/${filename}.dmb\0-trusted`,
-          new Map([['LD_LIBRARY_PATH', '/byond/bin']]),
-        );
-      })
-      .then((server) => {
-        this.output.emit('\n=== Run stage ===\n');
-        server.stderr.subscribe((val) => this.output.emit(val));
-        stageAbort = server.kill;
-        return once(server.exit);
-      })
-      .then(() => {
-        stageAbort = undefined;
-      })
-      .finally(() => {
-        if (stageAbort) stageAbort();
-        this.commandQueue.runProcess(
-          '/bin/rm',
-          `/mnt/host/${filename}.dme\0/mnt/host/${filename}.dmb`,
-        );
-      }, true);
+      const filename = `tmp-${Date.now()}`;
+      let stageAbort: Function | undefined = undefined;
 
-    signal?.addEventListener('abort', () => promise.cancel());
-    return promise;
+      this.reset.emit();
+      const promise = new CancelablePromise<void>((resolve) => {
+        resolve(this.emulator.start());
+      })
+        .then(() => {
+          return this.emulator.sendFile(filename + '.dme', code);
+        })
+        .then(() => {
+          return this.commandQueue.runProcess(
+            path + 'DreamMaker',
+            '/mnt/host/' + filename + '.dme',
+            new Map([['LD_LIBRARY_PATH', path]]),
+          );
+        })
+        .then((compiler) => {
+          this.output.emit('=== Compile stage ===\n');
+          compiler.stdout.subscribe((val) => this.output.emit(val));
+          stageAbort = compiler.kill;
+          return once(compiler.exit);
+        })
+        .then(() => {
+          return this.commandQueue.runProcess(
+            path + 'DreamDaemon',
+            `/mnt/host/${filename}.dmb\0-trusted`,
+            new Map([['LD_LIBRARY_PATH', path]]),
+          );
+        })
+        .then((server) => {
+          this.output.emit('\n=== Run stage ===\n');
+          server.stderr.subscribe((val) => this.output.emit(val));
+          stageAbort = server.kill;
+          return once(server.exit);
+        })
+        .then(() => {
+          stageAbort = undefined;
+        })
+        .finally(() => {
+          if (stageAbort) stageAbort();
+          this.commandQueue.runProcess(
+            '/bin/rm',
+            `/mnt/host/${filename}.dme\0/mnt/host/${filename}.dmb`,
+          );
+        }, true);
+
+      signal?.addEventListener('abort', () => promise.cancel());
+      return promise;
+    });
   }
 }
