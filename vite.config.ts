f2f05c950aef1f65c7c9bbce081ea2b2dc910c44
ZeWaka <zewakagamer@gmail.com>
2026-08-12 22:55:41 -0700

    warn

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { monaco } from '@bithero/monaco-editor-vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import packageJson from './package.json'
+import packageJson from './package.json' with { type: 'json' }
 
 const nodeCryptoStubPath = fileURLToPath(
   new URL('./src/utils/nodeCryptoStub.ts', import.meta.url)
